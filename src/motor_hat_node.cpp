86900e3f919f6d3ac9b8dcfcd5bcc4d6f2b2c821
mat kelcey (on pi) <matthew.kelcey+pi@gmail.com>
2016-03-31 05:52:49 +0000

    fix subscriber startup. clean up debug

--- src/motor_hat_node.cpp
+++ src/motor_hat_node.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/Int16MultiArray.h>
-#include <std_msgs/Int32.h>  // debug
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
@@ -42,23 +42,19 @@ int fd = 0;
 void i2c_setup() {
   fd = wiringPiI2CSetup(I2C_ADDR);
   if (fd == -1)
-    cerr << "no i2c device found?" << endl;
+    ROS_ERROR("Cant find I2C device with addr %d", I2C_ADDR);
 }
 
 void write(int reg, int data) {
-  cout << ">write(" << reg << "," << data << ")" << endl;
   wiringPiI2CWriteReg8(fd, reg, data);
 }
 
 int read(int reg) {
-  cout << ">read(" << reg << ")";
   int val = wiringPiI2CReadReg8(fd, reg);
-  cout << " returning " << val << endl;
   return val;
 }
 
 void setPWM(int channel, int on, int off) {
-  cout << ">setPWM(" << channel << "," << on << "," << off << ")" << endl;
   write(LED0_ON_L+4*channel, on & 0xFF);
   write(LED0_ON_H+4*channel, on >> 8);
   write(LED0_OFF_L+4*channel, off & 0xFF);
@@ -66,7 +62,6 @@ void setPWM(int channel, int on, int off) {
 }
 
 void setPin(int pin, int val) {
-  cout << ">setPin(" << pin << "," << val << ")" << endl;
   if (val == 0)
     setPWM(pin, 0, 4096);
   else if (val == 1)
@@ -81,7 +76,6 @@ void setPin(int pin, int val) {
 void init_hat() {
   // This is just cut and paste (including sleeps)
   // See original for some more explaination....
-  cout << ">init" << endl;
   i2c_setup();
   write(ALL_LED_ON_L, 0);
   write(ALL_LED_ON_H, 0);
@@ -91,15 +85,12 @@ void init_hat() {
   write(MODE1, ALLCALL);
   usleep(5000);
   int mode1 = read(MODE1);
-  cout << "mode1 = " << mode1 << endl;
   mode1 &= ~SLEEP;
   write(MODE1, mode1);
   usleep(5000);
-  cout << "<init" << endl;
 }
 
 void setPWMFreq(int freq) {
-  cout << ">setPWMFreq" << endl;
   float prescaleval = 25000000.0;
   prescaleval /= 4096.0;
   prescaleval /= freq;
@@ -112,21 +103,22 @@ void setPWMFreq(int freq) {
   write(MODE1, old_mode);
   usleep(5000);
   write(MODE1, old_mode | 0x80);
-  cout << "<setPWMFreq" << endl;
 }
 
 void stop(int motor_id) {
+  ROS_INFO("Stopping motor %d", motor_id);
   setPin(in1Pin[motor_id], 0);
   setPin(in2Pin[motor_id], 0);
 }
 
 void setSpeed(int motor_id, int speed) {
+  ROS_INFO("Setting speed of motor %d to %d", motor_id, speed);
   if (motor_id < 0 || motor_id > 3) {
-    cerr << "invalid motor_id " << motor_id;
+    ROS_ERROR("Invalid motor_id [%d] Expected 0,1,2,3", motor_id);
     return;
   }
   if (speed < -255 || speed > 255) {
-    cerr << "invalid speed " << speed;
+    ROS_ERROR("Invalid Speed [%d] Expected >=-255 <=255", speed);
     return;
   }
 
@@ -149,21 +141,16 @@ void setSpeed(int motor_id, int speed) {
 }
 
 void turnOffMotors() {
-  cout << ">turnOffMotors" << endl;
   for (int i = 0; i < 4; ++i)
     stop(i);
 }
 
 // Receive a vector4 of int corresponding to the desired motor speeds.
-//void cmdCallback(const std_msgs::Int16MultiArray::ConstPtr& msg) {
-void cmdCallback(const std_msgs::Int32::ConstPtr& msg) {
-  /*
-  cout << msg->data[0] << " " << msg->data[1] << " " << msg->data[2]
-       << " " << msg->data[3] << endl;
+void cmdCallback(const std_msgs::Int16MultiArray::ConstPtr& msg) {
+  // TODO: check msg->data length, currently if you send 2 values
+  //       defaults to 0.0 for 3rd and 4th value.
   for (int i = 0; i < 4; ++i)
     setSpeed(i, msg->data[i]);
-  */
-  cout << "CALLBACK " << msg->data << endl;
 }
 
 int main(int argc, char **argv) {
@@ -173,17 +160,11 @@ int main(int argc, char **argv) {
   atexit(turnOffMotors);
 
   // Start ROS node stuff.
-  cout << "s1" << endl;
   ros::init(argc, argv, "rover_motors");
-  cout << "s2" << endl;  
   ros::NodeHandle node;
-  cout << "s3" << endl;  
-  node.subscribe("rcmd", 5, cmdCallback);
-  cout << "s4" << endl;
+  ros::Subscriber sub = node.subscribe("cmd", 5, cmdCallback);
   
   // Run until exit.
   ros::spin();
   return 0;
 }
-
-
